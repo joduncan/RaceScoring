06f23833c78e244abc3024798faf1cbfb7cf08d8
David Hanley <dhanley@groupon.com>
2015-06-27 19:21:59 -0500

    bunch of progress; parses, loads races, assigns scores, yay!

--- src/scoring/core.clj
+++ src/scoring/core.clj
@@ -1,19 +1,63 @@
 (ns scoring.core
-    ;(:use [clojure.string])
+    (:require [clojure.string :as string])
+    (:require [clj-time.core :as t :only [date-time]])
+    ;(:require [clj-time.format :as f])
     (:require [clojure.data.csv :as csv]
 	      [clojure.java.io  :as io]))
 
-(use 'clojure.string)
 
 (defn l[] (use 'scoring.core :reload))
 
-(defn to-race-struct[fn data]
-  (println "loading" fn)
-  (let [itm #(trim (get (nth data %) 0))] 
-  {:name   (itm 0)
-   :date   (itm 1)
-   :url    (itm 2)
-   :points (Integer. (itm 3))
+(defn int-or-nil[data]
+  (try (Integer. data) (catch Exception e nil)))
+
+(defn parse-time-or-nil[ time ] time
+  (try
+   (let [[intpart dec](string/split time #"\.")
+	[ss mm hh](reverse (map #(Integer. %) (string/split intpart #":")))
+	[hours minutes seconds](map #(if (nil? %) 0 %) [hh mm ss])]
+	(+ (* 60 60 hours) (* 60 minutes) seconds (Float. (string/join "" ["0." dec]))))
+   (catch Exception e nil))
+  )
+  
+(defn to-sex[data]
+  (case (first (string/upper-case data))
+    \F :female
+    \M :male
+    nil))
+
+(defn list-getter[lst]
+  #(try (nth lst %) (catch Exception e "")))
+
+(defn row-to-athlete-result[row]
+  (let [itm (comp string/trim (list-getter row))]
+    {:name (itm 1)
+     :age  (int-or-nil (itm 2))
+     :sex  (to-sex (itm 3))
+     :time (parse-time-or-nil (itm 4)) } ))
+
+(defn scores[base]
+  "an infinite list of descending scores per rank starting with the base score"
+  (map (fn[i]{:score (* base (/ 5 (+ 5 i)))}) (range)))
+
+(defn ranking-list
+  "an infinite list of ranks, used for females, males, and overall" 
+  [key] (map (fn[i]{key (+ i 1)}) (range))) 
+
+(defn to-race-struct[filename data]
+  (prn "loading" filename)
+  (let [itm        (fn[i](string/trim (get (nth data i) 0)))
+        points     (Integer. (itm 3))
+	racers     (map merge (map row-to-athlete-result (drop 4 data)) (ranking-list :overall-rank))
+	score-list (scores points)
+	sexer      (fn[sex rank-key] (map merge (filter (fn[athlete](= (:sex athlete) sex)) racers) score-list (ranking-list rank-key)))
+ ]
+  {:name          (itm 0)
+   :date          (apply t/date-time (map #(Integer. %) (string/split (itm 1) #"-")))
+   :url           (itm 2)
+   :points        points
+   :male-racers   (sexer :male :male-rank)
+   :female-racers (sexer :female :female-rank)
    }))
   
 (defn load-race-data[fn]  
@@ -22,6 +66,8 @@
 
 (defn load-all-races[]
   (map load-race-data (rest (file-seq (java.io.File. "data")))))
+
+;(def race-data (load-all-races))
 
 (defn make-scores[ args ]
   (print "making scores"))
