5969ba36cb83ad4859a597817e7dd229b7c0f871
David Hanley <dhanley@groupon.com>
2015-07-14 09:36:28 -0500

    an athlete's result contains a reference to the race it was acheived in

--- src/scoring/core.clj
+++ src/scoring/core.clj
@@ -30,12 +30,13 @@
   #(try (nth lst %) (catch Exception e "")))
 
 ; TODO: flip it when the format is last, first 
-(defn row-to-athlete-result[row]
+(defn row-to-athlete-result[race-id row]
   (let [itm (comp string/trim (list-getter row))]
     {:name (itm 1)
      :age  (int-or-nil (itm 2))
      :sex  (to-sex (itm 3))
-     :time (parse-time-or-nil (itm 4)) } ))
+     :time (parse-time-or-nil (itm 4))
+     :race-id race-id} ))
 
 (defn scores[base]
   "an infinite list of descending scores per rank starting with the base score"
@@ -47,11 +48,11 @@
   [key] (map (fn[i]{key (+ i 1)}) (range))) 
 (def ranking-list (memoize ranking-list))
 
-(defn to-race-struct[filename data id]
+(defn to-race-struct[filename data race-id]
   ;(prn "loading" filename)
   (let [itm        (fn[i](string/trim (get (nth data i) 0)))
         points     (Integer. (itm 3))
-	racers     (map merge (map row-to-athlete-result (drop 4 data)) (ranking-list :overall-rank))
+	racers     (map merge (map (partial row-to-athlete-result race-id) (drop 4 data)) (ranking-list :overall-rank))
 	score-list (scores points)
 	sexer      (fn[sex rank-key] (map merge (filter (fn[athlete](= (:sex athlete) sex)) racers) score-list (ranking-list rank-key)))
  ]
@@ -61,7 +62,7 @@
    :points        points
    :male-racers   (sexer :male :male-rank)
    :female-racers (sexer :female :female-rank)
-   :race-id id
+   :race-id race-id
    }))
   
 (defn load-race-data[fn id]  
